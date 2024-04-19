a46bb77ec097eeaa575d050c4b968877e249e3ea
Matthew Pimentel <matthewpimentel@hotmail.com>
2024-04-19 11:00:18 -0400

    Scraped the team names and scores

---
+++ HockeyResultsNotifier/GameResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HockeyResultsNotifier
+{
+    public class GameResult
+    {
+        public string teamOne;
+        public string teamTwo;
+
+        public string teamOneScore;
+        public string teamTwoScore;
+
+        public GameResult(string team1, string team2, string teamOneScore, string teamTwoScore)
+        {
+            teamOne = team1;
+            teamTwo = team2;
+            this.teamOneScore = teamOneScore;
+            this.teamTwoScore = teamTwoScore;
+        }
+
+        public override string ToString()
+        {
+            return $"Team1: {teamOne}\nTeam2: {teamTwo}\nscore: {teamOneScore} - {teamTwoScore}";
+        }
+    }
+}
--- HockeyResultsNotifier/Program.cs
+++ HockeyResultsNotifier/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HockeyResultsNotifier;
+using HtmlAgilityPack;
+using System.Threading.Tasks.Sources;
+
+List<GameResult> results = new List<GameResult>();
+
+HtmlWeb web = new HtmlWeb();
+
+HtmlDocument document = web.Load("https://www.hockey-reference.com/boxscores/");
+
+var teamNames = document.DocumentNode.SelectNodes("//div[@id=\"wrap\"]//div[@class=\"game_summaries\"]//div/table/tbody/tr/td[not(contains(@class, \"gamelink\"))]/a");
+var scores = document.DocumentNode.SelectNodes("//div[@id=\"wrap\"]//div[@class=\"game_summaries\"]//div/table/tbody/tr/td[@class=\"right\"]");
+
+List<int> scoresToKeep = new List<int>();
+
+for (int x = 0; x < scores.Count; x++)
+{
+    if (x % 3 != 2) // Keep every score that is not at index 2, 5, 8, etc.
+    {
+        scoresToKeep.Add(x);
+    }
+}
+
+List<HtmlNode> filteredScores = new List<HtmlNode>();
+foreach (int index in scoresToKeep)
+{
+    filteredScores.Add(scores[index]);
+}
+
+for (int i = 0; i < teamNames.Count; i++)
+{
+    if(i % 2 == 1)
+    {
+        GameResult gameResult = new GameResult(teamNames[i-1].InnerHtml, teamNames[i].InnerHtml, filteredScores[i-1].InnerHtml , filteredScores[i].InnerHtml);
+        results.Add(gameResult);
+    }
+}
+
+foreach(GameResult gameResult in results)
+{
+   Console.WriteLine(gameResult.ToString());
+}
+
+
